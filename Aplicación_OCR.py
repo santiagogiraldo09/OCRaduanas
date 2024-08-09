ed4663633f400d9503b8d127e828b9809382b8b3
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-08-08 20:09:17 -0500

    Update Aplicación_OCR.py

--- Aplicación_OCR.py
+++ Aplicación_OCR.py
@@ -7,13 +7,13 @@ from azure.ai.formrecognizer import DocumentAnalysisClient
 from io import BytesIO
 from PIL import Image
 import requests
-from dotenv import load_dotenv
 
-#Carga de variables del entorno
-load_dotenv()
-endpoint = os.getenv("AZURE_ENDPOINT")
-key = os.getenv("AZURE_KEY")
-API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
+# Configuración de Azure Form Recognizer
+endpoint = "https://demoocr.cognitiveservices.azure.com/"
+key = "125c4bac6511481290029459b6cf96c2"
+
+# Clave de API de Google Maps
+API_KEY = 'AIzaSyAup1kQpy0W1gyaWOY2IoUl9VAHP_7pxYI'
 
 # URL de la API de Geocoding
 GEOCODING_URL = "https://maps.googleapis.com/maps/api/geocode/json"
@@ -85,12 +85,7 @@ def categorizar_zona(latitud, longitud):
 # Función para analizar el documento
 def analyze_document(file_path):
     document_analysis_client = DocumentAnalysisClient(endpoint=endpoint, credential=AzureKeyCredential(key))
-    with open(file_path, "rb") as f:
-        poller = document_analysis_client.begin_analyze_document("prebuilt-invoice", f)
-        result = poller.result()
-
-    document_data = {}
-    for field_name, field in result.documents[0].fields.items():
+	@@ -22,13 +94,27 @@ def analyze_document(file_path):
         document_data[field_name] = field.value if field else "0"
     return document_data
 
@@ -108,7 +103,7 @@ def extract_full_address(address_value):
         state = address_value.state if address_value.state else ""
     else:
         return "No address found"
-    
+
     address_parts = [f"{road} {house_number}".strip()]
     if city:
         address_parts.append(city)
@@ -118,13 +113,7 @@ def extract_full_address(address_value):
 
 # Función para normalizar la dirección
 def normalize_address(address):
-    address = re.sub(r'\b[Cc][Rr]+\b', 'Carrera', address)
-    address = re.sub(r'\b[Cc][Ll][Ll]?\b', 'Calle', address)
-    address = re.sub(r'\b[Aa][Vv][Ee]?\b', 'Avenida', address)
-    address = re.sub(r'\b[Dd][Gg][Ii][Aa][Gg]?[Oo]?[Nn]?[Aa]?[Ll]?\b', 'Diagonal', address)
-    address = re.sub(r'\b[Ii][Nn][Tt]?[Ee]?[Rr]?[Ii]?[Oo]?[Rr]?\b', 'Interior', address)
-    address = re.sub(r'[^a-zA-Z0-9\s#]', '', address)
-    address = re.sub(r'\s+', ' ', address).strip()
+	@@ -42,62 +128,70 @@ def normalize_address(address):
     return address
 
 # Función para guardar los resultados
@@ -140,11 +129,10 @@ def main():
     st.set_page_config(layout="centered")
     col1, col2 = st.columns([2, 1.5])
     col1.title("Analizador de Documentos")
-    image_path = os.path.join(os.path.dirname(__file__), 'LOGOTIPO IAC-02 1.png')  # Usar ruta relativa
-    image = Image.open(image_path)
+    image = Image.open(r"C:\Users\Lenovo Thinkpad E14\Downloads\LOGOTIPO IAC-02 1.png")  # Reemplaza con la ruta de tu imagen
     col2.image(image, width=300)
 
-    st.write("Carga documentos de RUT, Cámara de Comercio y Cotización para ser analizados.")
+    st.write("Carga documentos de Facturas, RUT y Cámara de Comercio para ser analizados.")
 
     # Cargar documentos
     rut_file = st.file_uploader("Carga RUT", type=["pdf", "jpg", "jpeg"])
@@ -164,24 +152,24 @@ def main():
 
                     try:
                         if doc_type == "Cámara de Comercio":
-                            street_address = "Carrera 63 B 32 E 25"
+                            street_address = "Carrera 63 B 32 E 25 OFICINA 206"
                         else:
                             data = analyze_document(file_path)
                             address_value = data.get("CustomerAddress", "No encontrado")
                             street_address = extract_full_address(address_value)
                             if doc_type == "Factura":
-                                direccion_factura = street_address  # Guardar la dirección del RUT para editar
+                                direccion_factura = street_address  # Guardar la dirección de la factura para editar
 
                         normalized_address = normalize_address(street_address)
                         formatted_data = {
-                            "Tipo de Documento": doc_type,
+                            "Document Type": doc_type,
                             "Vendor Name": data.get("VendorName", "No encontrado") if 'data' in locals() else "No encontrado",
                             "Customer Name": data.get("CustomerName", "No encontrado") if 'data' in locals() else "No encontrado",
                             "Dirección": normalized_address
                         }
                     except Exception as e:
                         formatted_data = {
-                            "Tipo de Documento": doc_type,
+                            "Document Type": doc_type,
                             "Vendor Name": "No encontrado",
                             "Customer Name": "No encontrado",
                             "Dirección": "Error normalizando dirección"
@@ -196,9 +184,7 @@ def main():
         st.write(df_results)
 
         # Verificar similitud de direcciones normalizadas
-        if len(set(all_normalized_addresses)) == 1:
-            st.success("Las direcciones son similares.")
-        else:
+	@@ -107,8 +201,31 @@ def main():
             st.warning("Las direcciones no coinciden.")
 
         # Guardar y permitir descarga
@@ -214,7 +200,7 @@ def main():
 
     if "direccion_factura" in st.session_state:
         st.title("Categorización de Dirección")
-        direccion_editada = st.text_input("Edita la dirección para categorizar", value=st.session_state.direccion_rut)
+        direccion_editada = st.text_input("Edita la dirección para categorizar", value=st.session_state.direccion_factura)
 
         if st.button("Confirmar y Categorizar"):
             if direccion_editada:
@@ -230,3 +216,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
