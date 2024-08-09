4d3d924759dd4dbdb23831ca9efa438340b055be
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-08-08 20:47:56 -0500

    Update Aplicación_OCR.py

--- Aplicación_OCR.py
+++ Aplicación_OCR.py
@@ -5,7 +5,6 @@ from azure.core.credentials import AzureKeyCredential
 from azure.ai.formrecognizer import DocumentAnalysisClient
 from io import BytesIO
 from PIL import Image
-import load_dotenv
 import requests
 import os
 
