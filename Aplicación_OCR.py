2aa60edc6bdefceb36662862c871178c458e2940
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-08-12 10:58:34 -0500

    Update Aplicación_OCR.py

--- Aplicación_OCR.py
+++ Aplicación_OCR.py
@@ -199,14 +199,13 @@ def main():
                             if doc_type == "RUT":
                                 direccion_rut = street_address  # Guardar la dirección del RUT para editar
 
-                        normalized_address = normalize_address(street_address)
                         base_address = clean_and_normalize_address(street_address)
 
                         formatted_data = {
                             "Document Type": doc_type,
                             "Vendor Name": data.get("VendorName", "No encontrado") if 'data' in locals() else "No encontrado",
                             "Customer Name": data.get("CustomerName", "No encontrado") if 'data' in locals() else "No encontrado",
-                            "Dirección": normalized_address,
+                            "Dirección": street_address,
                             "Depuración": base_address  # Añadimos la dirección depurada para compararla
                         }
                     except Exception as e:
@@ -261,6 +260,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
